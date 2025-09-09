de26d92c8a67e7c665123f1fb9759e8a6bf1bf5c
Edris9 <Edriskohestani1010@gmail.com>
2025-09-09 14:01:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PullrequestGithubAction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
